8915d2f20263902089aed276af66289876384bf5
milarrier <milandroid.beta@gmail.com>
2025-10-01 16:23:32 +0200

    changed SN() type from tf to ss for lsim() accuracy

--- lettucesimf.jl
+++ lettucesimf.jl
@@ -3,21 +3,8 @@ using ControlSystems
 using FFTW
 using Plots
 
-function pltSim(N::Int64,tend::Float64=5.0)
-    tw, vw = simF(N)
-    p = plot(tw, vw; ylims=(-2,2))
-    dt = 0.01;
-    t = 0:dt:tend;
-    u = ufn(t)
-    v,tout,x,uout = lsim(minreal(ss(SN(N))), u', t) # maybe use state-space model in SN()?
-    plot!(t, v[1,:], line=:dot);
-    return p
-end
-
-ufn(t) = sin.(10t)
-
+"simulates in freq domain then converts to time domain"
 function simF(N::Int64,tend::Float64=5.0)
-    # tend = 5
     nt = 2^23
     nt2 = div(nt,2)
     tpad = 1000*tend
@@ -30,11 +17,71 @@ function simF(N::Int64,tend::Float64=5.0)
     uf = fft(u)
     v = real(ifft(fftshift(vhat).*uf))
     it = floor(Int, tend/dt)
-    # plot(t[1:it],v[1:it]) # plot rendering is so slow for sizes like 2^23
+    # plot(t[1:it],v[1:it]) # plot rendering is so slow for the size of 2^23
     return t[1:it], v[1:it]
 end
 
-function pltFR(Ns)
+"frequency response of SN calculated node-wise and then summed up"
+function frSN(N::Int64,w)
+    nw = length(w)
+    r = zeros(nw)
+    p = tf(1, [0.1,1,0,0])
+    c = tf([2,1], [0.05,1])
+    # p = tf(1, [1,1])
+    # c = tf(1, [1,0])
+    g = p*c
+    for j = 0:2N
+        for k = 0:2N
+            Sjk = 1/(1+4g-2g*(cos(2π*j/(2N+1))+cos(2π*k/(2N+1))))
+            r = r + dropdims(freqresp(Sjk,w); dims=(1,2))
+        end
+    end
+    iw0 = findall(iszero, w) # need to handle zero frequency separately
+    if !isempty(iw0)
+        r[iw0[1]] = 1 # for s=0 only S00=1/1 survives the rest are 0/(0+c)
+    end
+    r = r/(2N+1)^2
+end
+
+#=============================== SANITY CHECKS ================================#
+"compares simF() result with time domain simulation for small N"
+function pltSim(N::Int64,tend::Float64=5.0)
+    tw, vw = simF(N)
+    p = plot(tw, vw)
+    dt = 0.01;
+    t = 0:dt:tend;
+    u = ufn(t)
+    v,tout,x,uout = lsim(SN(N), u', t)
+    plot!(t, v[1,:], line=:dot);
+    return p
+end
+
+# ufn(t) = sin.(10t)
+# ufn(t) = sin.(t)
+
+"finite sensitivity function in state-space form for lsim accuracy"
+function SN(N::Int64)
+    A = [0 1 0; 0 0 1; 0 0 -10]
+    B = [0; 0; 10]
+    C = [1 0 0]
+    Ak = -20
+    Bk = 1
+    Ck = -780
+    Dk = 40
+    p = ss(A,B,C,0.0)
+    c = ss(Ak,Bk,Ck,Dk)
+    g = minreal(p*c)
+    S = 0
+    for j = 0:2N
+        for k = 0:2N
+            S += 1/(1+4g-2g*(cos(2π*j/(2N+1))+cos(2π*k/(2N+1))))
+        end
+    end
+    return minreal(S)/(2N+1)^2
+end
+
+"a bunch of freq response curves for varying N on the same plot"
+function pltFR(Ns::Vector{Int64})
     p = plot()
     w = [10.0^t for t in range(-2.0,2.0,10000)]
     for N in Ns
@@ -49,44 +96,6 @@ function pltFR(Ns)
         display(p)
     end
     return p
-end
-
-"frequency response of SN calculated node-wise"
-function frSN(N::Int64,w)
-    nw = length(w)
-    iw0 = findall(iszero, w)
-    g = openloop()
-    r = zeros(nw) # need to handle zero frequency separately
-    for j = 0:2N
-        for k = 0:2N
-            Sjk = 1/(1+4g-2g*(cos(2π*j/(2N+1))+cos(2π*k/(2N+1))))
-            r = r + dropdims(freqresp(Sjk,w); dims=(1,2))
-        end
-    end
-    if !isempty(iw0)
-        r[iw0[1]] = 1
-    end
-    r = r/(2N+1)^2
-end
-
-function SN(N::Int64)
-    g = openloop()
-    S = 0
-    for j = 0:2N
-        for k = 0:2N
-            S += 1/(1+4g-2g*(cos(2π*j/(2N+1))+cos(2π*k/(2N+1))))
-        end
-    end
-    S = S/(2N+1)^2
-    return minreal(S)
-end
-
-function openloop()
-    p = tf(1, [0.1,1,0,0])
-    c = tf([2,1], [0.05,1])
-    # p = tf(1, [1,1])
-    # c = tf(1, [1,0])
-    g = p*c
 end
 
 # function pltFrSjk(N)
